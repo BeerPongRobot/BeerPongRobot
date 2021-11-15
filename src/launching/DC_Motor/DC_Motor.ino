e3137e14d2cbca2c93d749af539207af8b58c18e
Will Spencer <will.e.spencer@gmail.com>
2021-11-14 23:25:34 -0500

    Update components for new arduino board

--- src/launching/DC_Motor/DC_Motor.ino
+++ src/launching/DC_Motor/DC_Motor.ino
@@ -1,23 +1,30 @@
-#include <analogWrite.h>
+// #include <analogWrite.h> DO not include - made for esp32
 
 // pins DC motors connected to
-const int motorPin1 = SDA;
-const int motorPin2 = SCL;
+const int motorPin = A0;
+//const int motorPin2 = SCL;
 
 // emit a number between 0 and 255
-void emit_motor(int number) {
-  analogWrite(motorPin1, number);
-  analogWrite(motorPin2, number);
-}
+//void emit_motor(int number) {
+//  analogWrite(motorPin1, number);
+//}
 
 void setup() {
  // initialize the motors as outputs
- pinMode(motorPin1, OUTPUT);
- pinMode(motorPin2, OUTPUT);
+ pinMode(motorPin, OUTPUT);
  // Set up serial communication
  Serial.begin(9600);
 }
 
 void loop() {
-  emit_motor(100);
+  analogWrite(motorPin, 40); // there is some minimum we are not aware of
+//  for(int x = 20; x < 255; x+=20) {
+//    analogWrite(motorPin, x);
+//    delay(2000);
+//  }
+//  analogWrite(motorPin, 50);
+//  analogWrite(motorPin, 50);
+//  delay(5000);
+//  analogWrite(motorPin, 0);
+//  delay(5000);
 }
--- src/launching/Servo/Servo.ino
+++ src/launching/Servo/Servo.ino
@@ -4,16 +4,19 @@
 // create servo object to control a servo
 Servo myservo;
 // Pin to control the servo
-const int servo_pin = 12;
+const int servo_pin = 27;
 int val;
 
 void setup()
 {
-  myservo.attach(servo_pin);
+  myservo.attach(servo_pin, 600, 2400);
 }
 void loop()
 {
-  val = (val + 180) % 360;
-  myservo.write(val);
+  myservo.write(0);
+  delay(1000);
+  myservo.write(90);
+  delay(1000);
+  myservo.write(180);
   delay(1000);
 } 
